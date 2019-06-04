4828f8a2833868e75fa90962a4a7f3c10d0647c9
ChrisSheng97 <chrissheng97@gmail.com>
2019-06-04 16:29:45 -0700

    snapshot-baseline running

--- painters/train_cnn.py
+++ painters/train_cnn.py
@@ -1,5 +1,6 @@
 from os.path import join
 
+import math
 import keras.backend as K
 from keras.callbacks import ModelCheckpoint
 from keras.layers import Conv2D, Dense, Activation, MaxPooling2D
@@ -13,6 +14,8 @@ from data_provider import MODELS_DIR
 from data_provider import load_organized_data_info
 from data_provider import train_val_dirs_generators
 
+K.set_learning_phase(1)
+
 IMGS_DIM_3D = (3, 256, 256)
 CNN_MODEL_FILE = join(MODELS_DIR, 'cnn.h5')
 MAX_EPOCHS = 500
@@ -24,92 +27,95 @@ LAST_FEATURE_MAPS_SIZE = (128, 8, 8)
 PENULTIMATE_LAYER = 51
 PENULTIMATE_SIZE = 2048
 SOFTMAX_LAYER = 55
-SOFTMAX_SIZE = 1584
+SOFTMAX_SIZE = 1057
 
 
 def _train_model():
     data_info = load_organized_data_info(IMGS_DIM_3D[1])
+    print("data_info loaded")
     dir_tr = data_info['dir_tr']
     dir_val = data_info['dir_val']
 
     gen_tr, gen_val = train_val_dirs_generators(BATCH_SIZE, dir_tr, dir_val)
+    print("generators loaded")
     model = _cnn(IMGS_DIM_3D)
 
+    print(dir_tr, data_info['num_tr'], dir_val, data_info['num_val'])
     model.fit_generator(
         generator=gen_tr,
-        nb_epoch=MAX_EPOCHS,
-        samples_per_epoch=data_info['num_tr'],
+        epochs=MAX_EPOCHS,
+        steps_per_epoch=math.ceil(data_info['num_tr'] / BATCH_SIZE),
         validation_data=gen_val,
-        nb_val_samples=data_info['num_val'],
+        validation_steps=math.ceil(data_info['num_val'] / BATCH_SIZE),
         callbacks=[ModelCheckpoint(CNN_MODEL_FILE, save_best_only=True)],
-        verbose=2)
+        verbose=1)
 
 
 def _cnn(imgs_dim, compile_=True):
     model = Sequential()
 
     model.add(_convolutional_layer(nb_filter=16, input_shape=imgs_dim))
-    model.add(BatchNormalization(axis=1, mode=2))
-    model.add(PReLU(init=W_INIT))
+    model.add(BatchNormalization(axis=1))
+    model.add(PReLU(alpha_initializer=W_INIT))
     model.add(_convolutional_layer(nb_filter=16))
-    model.add(BatchNormalization(axis=1, mode=2))
-    model.add(PReLU(init=W_INIT))
+    model.add(BatchNormalization(axis=1))
+    model.add(PReLU(alpha_initializer=W_INIT))
     model.add(MaxPooling2D(pool_size=(2, 2)))
 
     model.add(_convolutional_layer(nb_filter=32))
-    model.add(BatchNormalization(axis=1, mode=2))
-    model.add(PReLU(init=W_INIT))
+    model.add(BatchNormalization(axis=1))
+    model.add(PReLU(alpha_initializer=W_INIT))
     model.add(_convolutional_layer(nb_filter=32))
-    model.add(BatchNormalization(axis=1, mode=2))
-    model.add(PReLU(init=W_INIT))
+    model.add(BatchNormalization(axis=1))
+    model.add(PReLU(alpha_initializer=W_INIT))
     model.add(_convolutional_layer(nb_filter=32))
-    model.add(BatchNormalization(axis=1, mode=2))
-    model.add(PReLU(init=W_INIT))
+    model.add(BatchNormalization(axis=1))
+    model.add(PReLU(alpha_initializer=W_INIT))
     model.add(MaxPooling2D(pool_size=(2, 2)))
 
     model.add(_convolutional_layer(nb_filter=64))
-    model.add(BatchNormalization(axis=1, mode=2))
-    model.add(PReLU(init=W_INIT))
+    model.add(BatchNormalization(axis=1))
+    model.add(PReLU(alpha_initializer=W_INIT))
     model.add(_convolutional_layer(nb_filter=64))
-    model.add(BatchNormalization(axis=1, mode=2))
-    model.add(PReLU(init=W_INIT))
+    model.add(BatchNormalization(axis=1))
+    model.add(PReLU(alpha_initializer=W_INIT))
     model.add(_convolutional_layer(nb_filter=64))
-    model.add(BatchNormalization(axis=1, mode=2))
-    model.add(PReLU(init=W_INIT))
+    model.add(BatchNormalization(axis=1))
+    model.add(PReLU(alpha_initializer=W_INIT))
     model.add(MaxPooling2D(pool_size=(2, 2)))
 
     model.add(_convolutional_layer(nb_filter=128))
-    model.add(BatchNormalization(axis=1, mode=2))
-    model.add(PReLU(init=W_INIT))
+    model.add(BatchNormalization(axis=1))
+    model.add(PReLU(alpha_initializer=W_INIT))
     model.add(_convolutional_layer(nb_filter=128))
-    model.add(BatchNormalization(axis=1, mode=2))
-    model.add(PReLU(init=W_INIT))
+    model.add(BatchNormalization(axis=1))
+    model.add(PReLU(alpha_initializer=W_INIT))
     model.add(_convolutional_layer(nb_filter=128))
-    model.add(BatchNormalization(axis=1, mode=2))
-    model.add(PReLU(init=W_INIT))
+    model.add(BatchNormalization(axis=1))
+    model.add(PReLU(alpha_initializer=W_INIT))
     model.add(MaxPooling2D(pool_size=(2, 2)))
 
     model.add(_convolutional_layer(nb_filter=256))
-    model.add(BatchNormalization(axis=1, mode=2))
-    model.add(PReLU(init=W_INIT))
+    model.add(BatchNormalization(axis=1))
+    model.add(PReLU(alpha_initializer=W_INIT))
     model.add(_convolutional_layer(nb_filter=256))
-    model.add(BatchNormalization(axis=1, mode=2))
-    model.add(PReLU(init=W_INIT))
+    model.add(BatchNormalization(axis=1))
+    model.add(PReLU(alpha_initializer=W_INIT))
     model.add(_convolutional_layer(nb_filter=256))
-    model.add(BatchNormalization(axis=1, mode=2))
-    model.add(PReLU(init=W_INIT))
+    model.add(BatchNormalization(axis=1))
+    model.add(PReLU(alpha_initializer=W_INIT))
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(p=0.5))
+    model.add(Dropout(rate=0.5))
 
     model.add(Flatten())
     model.add(_dense_layer(output_dim=PENULTIMATE_SIZE))
-    model.add(BatchNormalization(mode=2))
-    model.add(PReLU(init=W_INIT))
+    model.add(BatchNormalization())
+    model.add(PReLU(alpha_initializer=W_INIT))
 
     if compile_:
-        model.add(Dropout(p=0.5))
+        model.add(Dropout(rate=0.5))
         model.add(_dense_layer(output_dim=SOFTMAX_SIZE))
-        model.add(BatchNormalization(mode=2))
+        model.add(BatchNormalization())
         model.add(Activation(activation='softmax'))
         return compile_model(model)
 
@@ -125,22 +131,29 @@ def _convolutional_layer(nb_filter, input_shape=None):
 
 def _first_convolutional_layer(nb_filter, input_shape):
     return Conv2D(
-        nb_filter=nb_filter, nb_row=3, nb_col=3, input_shape=input_shape,
-        border_mode='same', init=W_INIT, W_regularizer=l2(l=L2_REG))
+        filters=nb_filter, kernel_size=(3, 3), input_shape=input_shape,
+        padding='same', kernel_initializer=W_INIT, kernel_regularizer=l2(l=L2_REG))
 
 
 def _intermediate_convolutional_layer(nb_filter):
     return Conv2D(
-        nb_filter=nb_filter, nb_row=3, nb_col=3, border_mode='same',
-        init=W_INIT, W_regularizer=l2(l=L2_REG))
+        filters=nb_filter, kernel_size=(3, 3), padding='same',
+        kernel_initializer=W_INIT, kernel_regularizer=l2(l=L2_REG))
 
 
 def _dense_layer(output_dim):
-    return Dense(output_dim=output_dim, W_regularizer=l2(l=L2_REG), init=W_INIT)
+    return Dense(units=output_dim, kernel_regularizer=l2(l=L2_REG), kernel_initializer=W_INIT)
 
 
 def compile_model(model):
     adam = Adam(lr=0.000074)
+
+    import tensorflow as tf
+    from keras.utils import multi_gpu_model
+    with tf.device('/device:CPU:0'):
+        origin_model = model
+    model = multi_gpu_model(origin_model, gpus=4)
+
     model.compile(
         loss='categorical_crossentropy',
         optimizer=adam,
